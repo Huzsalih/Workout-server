2dd456d75f4e7dd5c8edf0d4fd7a5b1ae66c0a08
Huzsalih <ssalihhuza123@gmail.com>
2025-02-07 12:19:19 +0300

    update server

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 
 let corsOptions = {
-  origin : ['http://localhost:5173']
+  origin : ['http://localhost:5173', 'https://workout-client-mauve.vercel.app/']
 }
 
 app.use(cors(corsOptions))
